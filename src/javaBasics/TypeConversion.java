9b0218ebd585caaa54a8f7cb3a1aee8b02e31e94
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2022-11-07 00:29:11 +0530

    type conversion

---
+++ src/javaBasics/TypeConversion.java
@@ -0,0 +1,13 @@
+package javaBasics;
+
+public class TypeConversion {
+    public static void main(String[] args) {
+
+     //   type conversion
+    //    byte-> short -> Int -> float -> long -> Double
+
+//        byte a =10;
+//        int b =a;
+//        System.out.println(b);
+    }
+}
---
+++ src/javaBasics/inputProduct.java
@@ -0,0 +1,13 @@
+package javaBasics;
+
+import java.util.Scanner;
+
+public class inputProduct {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int product = a*b;
+        System.out.println(product);
+    }
+}
