7aee6ff6bedd718e9f90cb330777270093c779e9
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2022-11-08 00:38:01 +0530

    type casting

---
+++ src/javaBasics/TypeCasting.java
@@ -0,0 +1,14 @@
+package javaBasics;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+
+        float a =25.12f;
+        int b = (int) (a);
+        System.out.println(b);
+
+        char ch ='m';
+        int no =ch;
+        System.out.println(no);
+    }
+}
