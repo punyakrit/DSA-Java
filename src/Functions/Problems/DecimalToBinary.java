e309d7aede5218f8c081763af46cd87c3f12c2f7
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2022-11-17 18:06:10 +0530

    Decimal Convertor

---
+++ src/Functions/Problems/DecimalToBinary.java
@@ -0,0 +1,23 @@
+package Functions.Problems;
+
+public class DecimalToBinary {
+
+    public static void decToBin(int n){
+        int myno =n;
+        int pow = 0;
+        int binNum = 0;
+        while (n> 0){
+            int rem = n %2;
+            binNum = binNum + (rem*(int) Math.pow(10,pow));
+            pow++;
+            n = n/2;
+        }
+        System.out.println("Binary form of " + myno + " =" + binNum);
+
+    }
+
+    public static void main(String[] args) {
+        decToBin(123);
+    }
+
+}
