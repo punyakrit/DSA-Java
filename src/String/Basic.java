aa86517898b3e2f3eca5416e86d98e0f09e2bd88
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2022-11-30 23:02:20 +0530

    string

---
+++ src/String/Basic.java
@@ -0,0 +1,27 @@
+package String;
+
+import java.util.Scanner;
+
+public class Basic {
+
+    public static void main(String[] args) {
+/*
+        char arr[] = {'a','b','c','d'};
+        String str = "abcd";
+        String str2 = new String("xyz");
+
+        //String are immutable
+        Scanner sc = new Scanner(System.in);
+
+        String single_name = sc.next();
+        System.out.println("name " +single_name);
+
+        String name = sc.nextLine();
+        System.out.println("full name : "+name);
+*/
+
+        String name= "Hello World";
+        System.out.println(name.length());
+
+    }
+}
