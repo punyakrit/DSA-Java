6750e084f03ab85dcccfded42e959a69f5728ec4
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2022-12-02 17:13:02 +0530

    bit

---
+++ src/BitWiseOperator/Basic.java
@@ -0,0 +1,18 @@
+package BitWiseOperator;
+
+public class Basic {
+
+    public static void main(String[] args) {
+        System.out.println(4 & 5);
+
+        System.out.println(5 | 6);
+
+        System.out.println(5 ^ 6);
+
+        System.out.println(~5);
+
+        System.out.println(5 << 2);
+
+        System.out.println( 6 >> 1 );
+    }
+}
